31df3bb9f74ec704621c77a1e21d7bdd167484fb
MrSepro <mohamadmahdi88gh@gmail.com>
2026-06-03 22:12:59 +0330

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,7 @@
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({
-    config: './tailwind.config.js'
-  })],
-  output: 'server',
-  adapter: undefined,
-  srcDir: './src',
-  publicDir: './public',
-  outDir: './dist',
-  server: {
-    port: 3001
-  },
+  output: 'hybrid',
+  adapter: netlify(),
 });
